d8cab2ae3b98250df7b32c8f4853104f08bb3cee
ProjectHubManager <marwanemajioui2@gmail.com>
2026-01-27 17:32:10 +0100

    Update PackFormModal.tsx

--- components/PackFormModal.tsx
+++ components/PackFormModal.tsx
@@ -1,7 +1,8 @@
-
 import React, { useState, useEffect } from 'react';
-import { Pack } from '../types';
-import { ENTITY_OPTIONS, PLATFORM_OPTIONS, TEAM_OPTIONS, STATUS_OPTIONS, LOCATION_OPTIONS } from '../constants';
+// FIX: Added .ts extension
+import { Pack } from '../types.ts';
+// FIX: Added .ts extension
+import { ENTITY_OPTIONS, PLATFORM_OPTIONS, TEAM_OPTIONS, STATUS_OPTIONS, LOCATION_OPTIONS } from '../constants.ts';
 
 interface PackFormModalProps {
   isOpen: boolean;
