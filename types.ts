572e89635cd275b6dd95b59b15d9eea86a7a0dd2
ProjectHubManager <marwanemajioui2@gmail.com>
2026-01-28 14:04:53 +0100

    Update types.ts

--- types.ts
+++ types.ts
@@ -1,32 +1,35 @@
 export interface Pack {
-  id: string;
-  entity: string;
-  entityProd: string;
-  platform: string;
-  team: string;
-  localExternal: string;
-  pack: string;
-  server: string;
-  rdps: string;
-  password?: string;
-  countProfiles: number | string;
-  interval: string;
-  status: string;
-  notes?: string;
-  dataSeeds: string;
-  backup: string;
+    id: string;
+    entity: string;
+    entityProd: string;
+    platform: string;
+    team: string;
+    localExternal: string;
+    pack: string;
+    dataSeeds: string;
+    server: string;
+    disk?: string;
+    rdps: string;
+    password?: string;
+    interval: string;
+    browser?: string;
+    status: string;
+    countProfiles: string | number;
+    backup: string;
+    totalSeedsOk?: string;
+    notes?: string;
 }
 
 export interface FilterState {
-  search: string;
-  entity: string;
-  status: string;
-  platform: string;
-  localExternal: string;
-  team: string;
+    search: string;
+    entity: string;
+    status: string;
+    platform: string;
+    localExternal: string;
+    team: string;
 }
 
 export interface SortConfig {
-  key: keyof Pack;
-  direction: 'asc' | 'desc';
+    key: keyof Pack;
+    direction: 'asc' | 'desc';
 }
